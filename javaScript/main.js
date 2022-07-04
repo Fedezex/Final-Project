19c984b9d99ad505e26e538c722297396ac1f0aa
FedeAr <fedezeqarancibia@gmail.com>
2022-07-04 19:04:05 -0300

    JS

--- javaScript/main.js
+++ javaScript/main.js
@@ -50,40 +50,40 @@ console.log(usuario);
 
 
 // Sweet Alert
-(async () => {
+// (async () => {
 
-const {value:provincia} = await Swal.fire({
-title:"¡Bienvenido!",
-text: "Selecciona tu provincia",
-confirmButtonText: "Seleccionar",
-footer: "Informacion requerida",
-padding: "1rem",
-backdrop: true,
-position: "center",
-allowOutsideClick: true,
-allowEscapeKey: false,
-allowEnterKey: false,
-stopKeydownPropagation: false,
-input: "select",
-inputPlaceholder: "Provincia",
-inputValue: "",
-inputOptions: {
-    tucuman: "Tucumán",
-    salta: "Salta",
-    jujuy: "Jujuy",
-    catamarca: "Catamarca",
-    santiago: "Santiago del Estero"
-},
-imageUrl: "image/Logo2.png",
-imageWidth: "200px",
-imageAlt: "Logotipo A4",
+// const {value:provincia} = await Swal.fire({
+// title:"¡Bienvenido!",
+// text: "Selecciona tu provincia",
+// confirmButtonText: "Seleccionar",
+// footer: "Informacion requerida",
+// padding: "1rem",
+// backdrop: true,
+// position: "center",
+// allowOutsideClick: true,
+// allowEscapeKey: false,
+// allowEnterKey: false,
+// stopKeydownPropagation: false,
+// input: "select",
+// inputPlaceholder: "Provincia",
+// inputValue: "",
+// inputOptions: {
+//     tucuman: "Tucumán",
+//     salta: "Salta",
+//     jujuy: "Jujuy",
+//     catamarca: "Catamarca",
+//     santiago: "Santiago del Estero"
+// },
+// imageUrl: "image/Logo2.png",
+// imageWidth: "200px",
+// imageAlt: "Logotipo A4",
 
-})
+// })
 
-if(provincia){
-Swal.fire({
-    title:`Seleccionaste ${provincia}`
-})
-}
+// if(provincia){
+// Swal.fire({
+//     title:`Seleccionaste ${provincia}`
+// })
+// }
 
-})()
+// })()
