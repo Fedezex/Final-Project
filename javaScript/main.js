4574474820ab51f62e0385242dfaf57d9da29259
FedeAr <fedezeqarancibia@gmail.com>
2022-07-10 10:35:01 -0300

    JS buttonUp

--- javaScript/main.js
+++ javaScript/main.js
@@ -7,5 +7,28 @@ const btnSwitch = document.querySelector("#switch");
     btnSwitch.classList.toggle('active');
 });
 
+// Scroll up
+
+document.getElementById("button-up-up").addEventListener("click",scrollUp);
+
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0) { 
+        window.scrollTo (0, 0);
+        
+    }
+}
+
+buttonUp = document.getElementById("button-up-up");
+
+window.onscroll = function(){
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+}
 
 
