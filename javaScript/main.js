df8a3ae6d9b974963d01201ced59ed0508bdf3db
FedeAr <fedezeqarancibia@gmail.com>
2022-07-10 22:04:23 -0300

    JS API Fetch

--- javaScript/main.js
+++ javaScript/main.js
@@ -33,5 +33,37 @@ window.onscroll = function(){
 
 
 // API Fetch
+const API_URL = 'http://jsonplaceholder.typicode.com';
+
+const HTMLResponse = document.querySelector("#app");
+const ul = document.createElement('ul');
 
 
+fetch(`${API_URL}/users`)
+.then((response) => response.json())
+.then((users) => {
+    users.forEach(user =>{
+        let elem = document.createElement('li');
+        elem.appendChild(
+            document.createTextNode(`${user.name} \u2709 ${user.email}`));
+        ul.appendChild(elem)
+    });
+    
+    HTMLResponse.appendChild(ul);
+});
+
+
+
+// const xhr = new XMLHttpRequest();
+
+// function onRequestHandler(){
+//     if(this.readyState == 4 && this.status == 200){
+//         const data = JSON.parse(this.response);
+//         const HTMLResponse = document.querySelector('#app');
+//         const tpl = data.map((user) => `<li>${user.name} ${user.email}</li>`);
+//         HTMLResponse.innerHTML = `<ul>${tpl}</ul>`
+//     }
+// }
+// xhr.addEventListener("load", onRequestHandler);
+// xhr.open("GET",`${API_URL}/users`);
+// xhr.send();
