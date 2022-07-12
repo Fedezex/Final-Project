16900a92216b5758d932c5dbcf0b6f711c2252f9
FedeAr <fedezeqarancibia@gmail.com>
2022-07-12 19:37:35 -0300

    JS API Fetch

--- javaScript/main.js
+++ javaScript/main.js
@@ -51,19 +51,3 @@ fetch(`${API_URL}/users`)
     
     HTMLResponse.appendChild(ul);
 });
-
-
-
-// const xhr = new XMLHttpRequest();
-
-// function onRequestHandler(){
-//     if(this.readyState == 4 && this.status == 200){
-//         const data = JSON.parse(this.response);
-//         const HTMLResponse = document.querySelector('#app');
-//         const tpl = data.map((user) => `<li>${user.name} ${user.email}</li>`);
-//         HTMLResponse.innerHTML = `<ul>${tpl}</ul>`
-//     }
-// }
-// xhr.addEventListener("load", onRequestHandler);
-// xhr.open("GET",`${API_URL}/users`);
-// xhr.send();
