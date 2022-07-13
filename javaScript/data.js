499f56fa1a8ea268d54e6088c23a910020244725
FedeAr <fedezeqarancibia@gmail.com>
2022-07-13 19:21:31 -0300

    JS Data Base

---
+++ javaScript/data.js
@@ -0,0 +1,38 @@
+[
+    {
+      "precio": 200,
+      "id": 1,
+      "title": "Envíos",
+      "thumbnailUrl": "https://picsum.photos/id/0/600"
+    },
+    {
+      "precio": 300,
+      "id": 2,
+      "title": "Laminado",
+      "thumbnailUrl": "https://picsum.photos/id/10/600"
+    },
+    {
+      "precio": 100,
+      "id": 3,
+      "title": "Espiralado",
+      "thumbnailUrl": "https://picsum.photos/id/20/600"
+    },
+    {
+      "precio": 500,
+      "id": 4,
+      "title": "Barniz Sectorizado",
+      "thumbnailUrl": "https://picsum.photos/id/30/600"
+    },
+    {
+      "precio": 250,
+      "id": 5,
+      "title": "Sistema de troquelado",
+      "thumbnailUrl": "https://picsum.photos/id/40/600"
+    },
+    {
+      "precio": 1500,
+      "id": 6,
+      "title": "Pre Impresión - CTP",
+      "thumbnailUrl": "https://picsum.photos/id/50/600"
+    }
+  ]
